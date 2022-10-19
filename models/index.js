b53db8cad3b0309d045129816e0fc9ee5b2b5b60
stephen <94320530+stephen-kern@users.noreply.github.com>
2022-10-18 20:57:06 -0500

    set up models, connection, and server

---
+++ models/index.js
@@ -0,0 +1,42 @@
+const User = require("./User");
+const Category = require("./Category");
+const Recipe = require("./Recipe");
+const RecipeDiet = require("./RecipeDiet");
+const Diet = require("./Diet");
+
+// User belongsTo RECIPES
+Recipe.belongsTo(User, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE",
+});
+
+// RECIPE belongsTo Category
+Recipe.belongsTo(Category, {
+  foreignKey: "category_id",
+  onDelete: "SET NULL",
+});
+
+// Categories have many RECIPES
+Category.hasMany(Recipe, {
+  foreignKey: "category_id",
+});
+
+// Products belongToMany Tags (through ProductTag)
+Recipe.belongsToMany(Diet, {
+  through: RecipeDiet,
+  foreignKey: "recipe_id",
+});
+
+// Tags belongToMany Products (through ProductTag)
+Diet.belongsToMany(Recipe, {
+  through: RecipeDiet,
+  foreignKey: "diet_id",
+});
+
+module.exports = {
+  Recipe,
+  Category,
+  Diet,
+  RecipeDiet,
+  User,
+};
